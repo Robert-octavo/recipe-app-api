d46cf9cfabc472a7abbcb41ce069417710883e59
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-11-27 18:39:33 -0500

    add the test to see the info from a user

--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -29,3 +29,11 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works"""
+        # 127.0.0.1:8000/admin/core/user/1/change/
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
